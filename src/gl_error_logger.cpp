84f434944c9da86befc1fa08511c9cf66d35bfd1
Josefu <joemilsom@gmail.com>
2025-03-24 12:55:57 +1100

    Add error logger util

---
+++ src/gl_error_logger.cpp
@@ -0,0 +1,16 @@
+#include "gl_error_logger.h"
+#include "logger.h"
+
+void ErrorLogger::LogGlError()
+{
+  auto error = glGetError();
+  switch (error)
+  {
+  case 0x0502:
+    LOG_ERROR("GL error 0x0502: Invalid operation");
+    break;
+  default:
+    LOG_ERROR("GL error {}", error);
+    break;
+  }
+}
---
+++ src/gl_error_logger.h
@@ -0,0 +1,4 @@
+namespace ErrorLogger
+{
+  void LogGlError();
+}
